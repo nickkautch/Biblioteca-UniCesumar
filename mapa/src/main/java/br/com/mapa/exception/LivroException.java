479878205fbc2b5960f839ce86d04e17230d35a1
nickkautch <kautchnicole@gmail.com>
2025-07-10 17:58:30 -0300

    Delete mapa directory

--- mapa/src/main/java/br/com/mapa/exception/LivroException.java
+++
@@ -1,9 +0,0 @@
-package br.com.mapa.exception;
-
-//Exceção para tratar erros relacionados ao cadastro de livros
-
-public class LivroException extends Exception{
-    public LivroException(String msg){
-        super(msg);
-    }
-}
